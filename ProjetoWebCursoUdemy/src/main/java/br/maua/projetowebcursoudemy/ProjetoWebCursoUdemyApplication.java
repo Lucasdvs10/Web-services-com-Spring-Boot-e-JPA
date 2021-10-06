cf6391bc2ff03305c837d03792a4563de4e8fd91
Lucas Duez <lucas.verzaro@gmail.com>
2021-10-06 11:13:03 -0300

    Commit inicial

---
+++ ProjetoWebCursoUdemy/src/main/java/br/maua/projetowebcursoudemy/ProjetoWebCursoUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.maua.projetowebcursoudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebCursoUdemyApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(ProjetoWebCursoUdemyApplication.class, args);
+    }
+
+}
---
+++ ProjetoWebCursoUdemy/src/test/java/br/maua/projetowebcursoudemy/ProjetoWebCursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.maua.projetowebcursoudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebCursoUdemyApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
